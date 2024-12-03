f419eceb1c09b65bf550c582e740c9389d9434f8
DavidDevGt <josuedavidvl18@gmail.com>
2024-12-02 22:34:52 -0600

    Add functionality and update styles for dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
 
-  toggleDarkMode() {
-    document.body.classList.toggle('dark');
+  isDarkMode = false;
+  constructor() {
+    this.setInitialTheme();
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    document.documentElement.setAttribute('theme', this.isDarkMode ? 'dark' : 'light');
+  }
+
+  private setInitialTheme() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    this.isDarkMode = prefersDark;
+    document.documentElement.setAttribute('theme', prefersDark ? 'dark' : 'light');
   }
   
 }
